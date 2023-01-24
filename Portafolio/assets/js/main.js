b5f444e4ea9788ac5a98a978521f29c12d711a3d
Paola-mh <122296968+Paola-mh@users.noreply.github.com>
2023-01-24 11:04:30 -0300

    Delete Portafolio directory

--- Portafolio/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-//Modifica alerta al enviar mensaje en contacto usando Sweetalert
-function enviCont() {
-    var text = document.getElementById("mensaje").value;
-    if (text != "") {
-      swal("Supercompara", "Mensaje enviado con exito ", "success", {button: "Vale!"});
-    } else {
-      swal("Supercompara", "No se ha enviado el mensaje", "error");
-    }
-}
